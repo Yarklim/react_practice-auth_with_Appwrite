e0265d28b75cfb8f533c81292cc36a8f6eda135f
Yaroslav Klimas <yarklim@gmail.com>
2023-10-15 12:44:13 +0300

    created privateRoutes

--- src/appwriteConfig.js
+++ src/appwriteConfig.js
@@ -1,1 +1,11 @@
-import { Client } from 'appwrite';
+import { Client, Account } from 'appwrite';
+
+const client = new Client();
+
+client
+  .setEndpoint('https://cloud.appwrite.io/v1')
+  .setProject('652b940f23de17696219');
+
+export const account = new Account(client);
+
+export default client;
